3c9baca2f72b4ff31412815df26384976f0f3bf2
Bastien Doreau <bdoreau@yahoo.fr>
2015-03-24 15:40:14 +0100

    Premier Commit

---
+++ src/main/java/com/humanbooster/TEDI/ThiViSteBa/SuperProf/App.java
@@ -0,0 +1,13 @@
+package com.humanbooster.TEDI.ThiViSteBa.SuperProf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
