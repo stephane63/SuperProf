146b956cf85b7ff3380e61d50029598f0d57621a
Stephane LIAUZU <stephane.liauzu@neuf.fr>
2015-03-24 16:12:03 +0100

    Pour tester les fonctions git sous eclipse

---
+++ src/main/java/com/humanbooster/TEDI/ThiViSteBa/SuperProf/Youpi.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.humanbooster.TEDI.ThiViSteBa.SuperProf;
+
+/**
+ * @author stephane
+ *
+ */
+public class Youpi {
+
+	/**
+	 * 
+	 */
+	public Youpi() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
