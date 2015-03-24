595eba2f970ccf5c9804a578b60095c1f8353aeb
Stephane LIAUZU <stephane.liauzu@neuf.fr>
2015-03-24 16:31:49 +0100

    Test apres changement

--- src/main/java/com/humanbooster/TEDI/ThiViSteBa/SuperProf/Youpi.java
+++ src/main/java/com/humanbooster/TEDI/ThiViSteBa/SuperProf/Youpi.java
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  */
 package com.humanbooster.TEDI.ThiViSteBa.SuperProf;
 
 /**
- * @author stephane
+ * @author stephane liauzu
  *
  */
 public class Youpi {
 
 	/**
-	 * 
+	 *
 	 */
 	public Youpi() {
 		// TODO Auto-generated constructor stub
